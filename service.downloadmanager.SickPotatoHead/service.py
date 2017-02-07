ea2545a22bbb154563513d2fc062d2682e0f5d30
lsellens <lsellens@gmail.com>
2017-02-07 16:38:34 -0600

    service was not stopping

--- service.downloadmanager.SickPotatoHead/service.py
+++ service.downloadmanager.SickPotatoHead/service.py
@@ -14,9 +14,7 @@ if __name__ == '__main__':
     monitor = xbmc.Monitor()
     
     while not monitor.abortRequested():
-        # Sleep/wait for abort for 10 seconds
-        if monitor.waitForAbort(10):
-            # Abort was requested while waiting. We should exit
-            break    
-    # Shutdown SickPotatoHead
-    system("kill `ps | grep -E 'python.*service.downloadmanager.SickPotatoHead' | awk '{print $1}'`")
+        if monitor.waitForAbort():
+            # Shutdown SickPotatoHead
+            system("kill `ps | grep -E 'python.*service.downloadmanager.SickPotatoHead' | awk '{print $1}'`")
+            break
